defbf5164fd3079b9917e108a6ab3cb78aa2a67e
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-07-04 10:54:29 +0530

    Add files via upload

---
+++ slice1.py
@@ -0,0 +1,24 @@
+# a='department'
+# print(a[:])
+# print(a[0:])
+# print(a[0:6])
+# print(a[6:])
+# print(a[4:])  #invalid syntex
+# print(a[4:10])   # invalid syntex
+# print(a[4:9])'
+a="department"
+# print(a[0:9])
+# print(a[4:10])
+# print(a[2:2])
+# print(a[5:2])
+# print(a[:])
+# print(a[:6])
+# print(a[5:4])
+# print(a[5:3])  # blonkline
+# print(a[2:2])   # blonkline 
+# print(a[0])
+# print(a[-1])
+# print(a[2])
+# print(a[-6])
+# print(a[-9:-1])
+# print(a[-9:-5])
